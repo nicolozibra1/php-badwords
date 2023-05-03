638d2bbc439d80615dfc3ba099e29c077c03e0af
nicolozibra1 <nico.zibra@gmail.com>
2023-05-03 16:25:24 +0200

    upgrade-layout

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<body>
-    <form action="result.php" method="POST">
-        <label for="paragraph">Insert your text</label>
-        <textarea name="paragraph" id="paragraph" cols="30" rows="10"></textarea>
-        <label for="word">Insert your word</label>
-        <input type="text" name="word" id="word">
-        <button type="submit">Send</button>
-    </form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PHP Badwords</title>
+</head>
+<body id="index">
+    <header class="d-flex flex-column align-items-center">
+        <div class="box-logo">
+            <img src="./img/logo.png" alt="logo">
+        </div>
+        <h1 class="title">PHP <span class="text-decoration-line-through fw-lighter">Badwords</span></h1>
+    </header>
+    <main class="container">
+        <form action="result.php" method="POST" class="d-flex flex-column align-items-center justify-content-center mt-3">
+            <label for="paragraph">Insert your text</label>
+            <textarea name="paragraph" id="paragraph" cols="30" rows="10" class="w-25"></textarea>
+            <label for="word">Insert BAD word</label>
+            <input type="text" name="word" id="word" class="w-25">
+            <button type="submit">SEND</button>
+        </form>
+    </main>
 </body>
+</html>
--- result.php
+++ result.php
@@ -10,14 +10,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP Badwords Calling</title>
 </head>
-<body>
-    <h1>This is your paragraph:</h1>
-    <span> <?php echo "$paragraph" ?> </span>
-    <span> <?php echo 'The paragraph lenght is:' . ' ' . strlen($paragraph) . ' ' . 'characters' ?> </span>
-    <h1>This is the new paragraph:</h1>
-    <span> <?php echo "$newParagraph" ?> </span>
-    <span> <?php echo 'The new paragraph lenght is:' . ' ' . strlen($newParagraph) . ' ' . 'characters' ?> </span>
+<body id="result">
+    <header class="d-flex flex-column align-items-center">
+        <div class="box-logo">
+            <img src="./img/logo.png" alt="logo">
+        </div>
+        <h1 class="title">PHP <span class="text-decoration-line-through fw-lighter">Badwords</span></h1>
+    </header>
+    <main class="container d-flex flex-column align-items-center justify-content-center mt-3">
+        <div class="box-old d-flex flex-column align-items-center justify-content-center">
+            <h4 class="fst-italic text-white">This is your paragraph:</h4>
+            <span class="fw-semibold fs-3"> <?php echo "$paragraph" ?> </span>
+            <span class="text-decoration-underline"> <?php echo 'The paragraph lenght is:' . ' ' . strlen($paragraph) . ' ' . 'characters' ?> </span>
+        </div>
+        <div class="box-new d-flex flex-column align-items-center justify-content-center">
+            <h4 class="fst-italic text-white">This is the NEW paragraph:</h4>
+            <span class="fw-semibold fs-3"> <?php echo "$newParagraph" ?> </span>
+            <span class="text-decoration-underline"> <?php echo 'The new paragraph lenght is:' . ' ' . strlen($newParagraph) . ' ' . 'characters' ?> </span>
+        </div>
+    </main>
+    <hr class="container w-25">
+    <footer class="container d-flex flex-column align-items-center justify-content-center mt-5">
+        <span class="fst-italic text-white">Thank you for visiting this page!</span>
+        <h5>Made with &hearts; by Nicolò Zibra </h5>
+    </footer>
 </body>
 </html>
